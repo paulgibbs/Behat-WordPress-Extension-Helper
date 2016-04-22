e50e3780692ebdad3d2d9089eb92678c63cf5390
Paul Gibbs <djpaul@gmail.com>
2016-04-22 20:13:10 +1000

    Modify SQL statements to support transactions.

--- behat-wordpress-extension-helper.php
+++ behat-wordpress-extension-helper.php
@@ -8,3 +8,23 @@
  * Plugin URI: https://github.com/paulgibbs/behat-wordpress-extension-helper/
  * License: GPL2+
  */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Modify SQL statements to support transactions.
+ *
+ * @param string $query
+ */
+function bweh_create_temporary_tables( $query ) {
+	if ( 'CREATE TABLE' === substr( trim( $query ), 0, 12 ) ) {
+		return substr_replace( trim( $query ), 'CREATE TEMPORARY TABLE', 0, 12 );
+
+	} elseif ( 'DROP TABLE' === substr( trim( $query ), 0, 10 ) ) {
+		return substr_replace( trim( $query ), 'DROP TEMPORARY TABLE', 0, 10 );
+	}
+
+	return $query;
+}
+add_filter( 'query', 'bweh_support_mysql_transactions' );
